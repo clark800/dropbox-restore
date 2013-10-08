eb9e9feb6dd6a12e9add6031967f62abbaa713ad
Chris Clark <clark800@gmail.com>
2013-10-08 16:15:43 -0400

    Avoid calling raw_input in python 3

--- restore.py
+++ restore.py
@@ -17,7 +17,11 @@ def authorize():
     print('1. Go to: ' + authorize_url)
     print('2. Click "Allow" (you might have to log in first)')
     print('3. Copy the authorization code.')
-    code = raw_input("Enter the authorization code here: ").strip()
+    try:
+        input = raw_input
+    except NameError:
+        pass
+    code = input("Enter the authorization code here: ").strip()
     access_token, user_id = flow.finish(code)
     return access_token
 
