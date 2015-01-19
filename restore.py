932db02dd5f61e324d0d37fdfbb5ab8c81eb8199
Tilman Vogel <tilman.vogel@web.de>
2015-01-19 17:00:03 +0100

    remove original app key

--- restore.py
+++ restore.py
@@ -2,8 +2,8 @@
 import sys, os, dropbox, time
 from datetime import datetime
 
-APP_KEY = 'hacwza866qep9o6'   # INSERT APP_KEY HERE
-APP_SECRET = 'kgipko61g58n6uc'     # INSERT APP_SECRET HERE
+APP_KEY = ''   # INSERT APP_KEY HERE
+APP_SECRET = ''     # INSERT APP_SECRET HERE
 DELAY = 0.2 # delay between each file (try to stay under API rate limits)
 
 HELP_MESSAGE = \
